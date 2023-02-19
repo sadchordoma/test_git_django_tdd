f08293ef7c1dd63dc92c5418283b4fbbf1c3dc96
happy <sadchordoma@gmail.com>
2023-02-19 22:09:16 +0500

    First FT specced out in comments, and now uses unittest.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_check_title_site(self):
+        # Херцог захотел узнать какие же ачивки или статьи есть у Люка.
+        # Он заходит на домашнюю страницу сайта
+        self.browser.get("http://localhost:8000")
+
+        # Он замечает, что на заголовок у страницы "Home | sadchordoma"
+        self.assertIn("Home | sadchordoma", self.browser.title)
+
+        # Он видит, что на главной странице есть статьи, кликнув на которые он
+        # переходит на страницу отдельной статьи
+
+
+if __name__ == "__main__":
+    unittest.main()
